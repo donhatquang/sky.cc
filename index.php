4a4a0fde7079aeac2f04709aa4528e2cfcaa487e
do nhat quang <donhatquang@gmail.com>
2015-10-22 02:04:37 +0800

    start
    start

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>Sky Chat - Viettel ICT</title>
+<link rel="stylesheet" href="Css/style.css" media="screen" type="text/css" />
+<link rel="stylesheet" href="Css/animate.css" media="screen" type="text/css" />
+<link rel="shortcut icon" href="Images/favicon.ico">
+</head>
+<body>
+<div class="wrap">
+  <h1 class="logo wow bounceIn"><img src="Images/logo.png"  alt="sky" /></h1>
+  <div class="text-intro">
+  	 <div>
+    <p class="wow bounceInLeft">天空是彩信的应用程序，即时翻译，帮助用户克服任何语言障碍</p>
+    <p class="wow bounceInLeft">支持12种语言，包括越南语，英语，法语，德语，意大利，西班牙，印度，中国，日本，韩国，柬埔寨和老挝的直接翻译</p>
+	<span class="arr2 wow bounceInRight"><img src="Images/arr2.png" alt="arrow" /></span>
+	</div>
+  </div>
+  <div class="btn-down"> 
+  	 <div class="btn-down-div">
+  	 <h2 class="wow fadeInDown">免费下载</h2>	
+	 <p class="wow bounceInUp"><a href="http://install.diawi.com/Yw2tM9" title="Download cho IOS"><img src="Images/ico-ios.png"/></a></p> 
+  	 <!--<p class="wow bounceInUp"><a href="resources/sky_1.0.31.apk" title="Download cho Android"><img src="Images/ico-google.png"/></a></p>-->
+		<p class="wow bounceInUp"><a href="https://play.google.com/store/apps/details?id=com.viettel.sky" title="Download cho Android"><img src="Images/ico-google.png"/></a></p>
+	 
+	 <span class="arr1 wow fadeInDown"><img src="Images/arr.png" alt="arrow" /></span>
+	 </div>
+   </div>
+  <div class="hphone wow bounceInRight"><img src="Images/phone-top.png" alt=""/></div>
+  
+</div>
+<div class="copy">A product of Viettel ICT © 2015</div>
+<script src="JS/jquery-1.9.0.min.js" type="text/javascript"></script>
+<script src="JS/wow.min.js"></script>
+<script>
+		 new WOW().init();
+
+
+		</script>
+
+<!-- Visit Counter  -->
+<div style="display:none;">
+<script src="http://s95.cnzz.com/z_stat.php?id=1256610121&web_id=1256610121" language="JavaScript"></script>
+
+<?php 
+require_once 'cs.php';
+echo '<img src="'._cnzzTrackPageView(1256610121).'" width="0" height="0"/>';
+?>
+</div>
+
+</body>
+</html>
