f661ae12d82aa6a4429b94d81dbc597a96711037
do nhat quang <donhatquang@gmail.com>
2015-10-22 02:25:01 +0800

    geo

--- index.php
+++ index.php
@@ -11,21 +11,21 @@
 <div class="wrap">
   <h1 class="logo wow bounceIn"><img src="Images/logo.png"  alt="sky" /></h1>
   <div class="text-intro">
-  	 <div>
+     <div>
     <p class="wow bounceInLeft">天空是彩信的应用程序，即时翻译，帮助用户克服任何语言障碍</p>
     <p class="wow bounceInLeft">支持12种语言，包括越南语，英语，法语，德语，意大利，西班牙，印度，中国，日本，韩国，柬埔寨和老挝的直接翻译</p>
-	<span class="arr2 wow bounceInRight"><img src="Images/arr2.png" alt="arrow" /></span>
-	</div>
+  <span class="arr2 wow bounceInRight"><img src="Images/arr2.png" alt="arrow" /></span>
+  </div>
   </div>
   <div class="btn-down"> 
-  	 <div class="btn-down-div">
-  	 <h2 class="wow fadeInDown">免费下载</h2>	
-	 <p class="wow bounceInUp"><a href="http://install.diawi.com/Yw2tM9" title="Download cho IOS"><img src="Images/ico-ios.png"/></a></p> 
-  	 <!--<p class="wow bounceInUp"><a href="resources/sky_1.0.31.apk" title="Download cho Android"><img src="Images/ico-google.png"/></a></p>-->
-		<p class="wow bounceInUp"><a href="https://play.google.com/store/apps/details?id=com.viettel.sky" title="Download cho Android"><img src="Images/ico-google.png"/></a></p>
-	 
-	 <span class="arr1 wow fadeInDown"><img src="Images/arr.png" alt="arrow" /></span>
-	 </div>
+     <div class="btn-down-div">
+     <h2 class="wow fadeInDown">免费下载</h2> 
+   <p class="wow bounceInUp"><a href="http://install.diawi.com/Yw2tM9" title="Download cho IOS"><img src="Images/ico-ios.png"/></a></p> 
+     <!--<p class="wow bounceInUp"><a href="resources/sky_1.0.31.apk" title="Download cho Android"><img src="Images/ico-google.png"/></a></p>-->
+    <p class="wow bounceInUp"><a href="https://play.google.com/store/apps/details?id=com.viettel.sky" title="Download cho Android"><img src="Images/ico-google.png"/></a></p>
+   
+   <span class="arr1 wow fadeInDown"><img src="Images/arr.png" alt="arrow" /></span>
+   </div>
    </div>
   <div class="hphone wow bounceInRight"><img src="Images/phone-top.png" alt=""/></div>
   
@@ -34,18 +34,20 @@
 <script src="JS/jquery-1.9.0.min.js" type="text/javascript"></script>
 <script src="JS/wow.min.js"></script>
 <script>
-		 new WOW().init();
+     new WOW().init();
 
 
-		</script>
+    </script>
 
 <!-- Visit Counter  -->
-<div style="display:none;">
+<div style="display:default;">
 <script src="http://s95.cnzz.com/z_stat.php?id=1256610121&web_id=1256610121" language="JavaScript"></script>
 
 <?php 
 require_once 'cs.php';
 echo '<img src="'._cnzzTrackPageView(1256610121).'" width="0" height="0"/>';
+echo "sky.cc";
+
 ?>
 </div>
 
